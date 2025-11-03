56618668efc4474effb15375f7fb58cae73aebcd
Nitish <tnitish654@gmail.com>
2025-11-03 20:10:54 +0800

    ✨ Add Google Sheets integration ✨ Added function to map current date to respective row on google sheets

--- src/index.ts
+++ src/index.ts
@@ -11,12 +11,34 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
-import { getAccessToken } from './strava-auth';
+import { DateTime } from 'luxon';
+import { getGoogleSheetData } from './auth/google-sheet-auth';
+import { getAccessToken } from './auth/strava-auth';
+import { getRowLabelFromDate } from './date-mapping';
 
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
+		const currentDate = DateTime.now().startOf('day');
 		const accessToken = await getAccessToken();
 
-		return new Response('Your Strava access token is: ' + accessToken);
+		const currentDateMapping = getRowLabelFromDate(currentDate);
+		console.log('>>> Current Date Mapping:', currentDateMapping);
+
+		const armyMarathonDocData = getGoogleSheetData(env, env.GOOGLE_SHEETS_ID);
+		await armyMarathonDocData.loadInfo();
+
+		const sheet = armyMarathonDocData.sheetsByIndex[0];
+		const rows = await sheet.getRows();
+
+		const nameList = rows.map((row) => row.get('NAME'));
+
+		return new Response(
+			JSON.stringify({
+				currentDate: currentDate.toISO(),
+				currentDateMapping,
+				accessToken,
+			}),
+			{ status: 200, headers: { 'Content-Type': 'application/json' } }
+		);
 	},
 } satisfies ExportedHandler<Env>;
